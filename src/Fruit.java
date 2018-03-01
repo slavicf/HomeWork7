d05b35ae01db95a03fd36985617effcae1c2476b
slavicf <jaroslavfrunt@gmail.com>
2018-03-01 11:20:58 +0200

    Fill project

---
+++ src/Fruit.java
@@ -0,0 +1,8 @@
+public class Fruit {                                        // класс фрукта
+
+    FruitType fruitType;                                    // вид фрукта (Apple, Apricot, Banana, Lemon, Mango,
+                                                                        // Orange, Peach, Pear, Plum, Strawberry)
+    int expiration;                                         // срок годности
+    String deliveryDate;                                    // дата поставки
+    double price;                                           // цена
+}
